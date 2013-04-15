df0a6f917181bf48b5803891b21650ee942b4f84
Gleb Lebedev <gleb@gleblebedev.com>
2013-04-15 19:29:06 +0400

    lua test

---
+++ src/cpp/tests/TestLua/test1.lua
@@ -0,0 +1,3 @@
+BaseClass = {}
+SubClass = {}
+
--- src/cpp/tests/TestLua/testlua.cpp
+++ src/cpp/tests/TestLua/testlua.cpp
@@ -1,20 +1,43 @@
 #include <toecore.h>
 #include <toemsgreg.h>
 #include <stdio.h>
+#include <stdlib.h>
+
 extern "C"{
 #include <lua.h>
+#include <lualib.h>
 #include <lauxlib.h>
 }
 
 #define ASSERT(flag, msg) { if (!(flag)) printf("Test fail: %s", msg); if (L) lua_close(L); return 1; }
 
+void bail(lua_State *L, char *msg){
+	fprintf(stderr, "\nFATAL ERROR:\n  %s: %s\n\n",
+		msg, lua_tostring(L, -1));
+	exit(1);
+}
+
+int pmain(lua_State *L)
+{
+	return 0;
+}
 
 int Test1()
 {
 	lua_State* L;
 	L=luaL_newstate();
-	ASSERT(L,"cannot create state: not enough memory")
-	//lua_pushcfunction(L,&pmain);
+	//ASSERT(L,"cannot create state: not enough memory")
+
+	lua_pushcclosure(L,&pmain,0);
+	lua_setglobal(L,"pmain");
+
+	luaL_openlibs(L);
+
+	if (luaL_loadfile(L, "test1.lua")) bail(L, "luaL_loadfile() failed");
+	lua_call(L,0,0);
+
+
+
 	//lua_pushinteger(L,argc);
 	//lua_pushlightuserdata(L,argv);
 	//if (lua_pcall(L,2,0,0)!=LUA_OK) fatal(lua_tostring(L,-1));
