fe54ac0453ec003f99dfe61b5bf1aa7beb3b1884
Gleb Lebedev <glebedev@netvoxlab.ru>
2013-08-19 12:56:20 +0400

    UTF

--- src/dotnet/NuGet/Toe.CirculArarrayQueue/ExtensionMethods.cs
+++ src/dotnet/NuGet/Toe.CirculArarrayQueue/ExtensionMethods.cs
@@ -34,39 +34,45 @@ namespace Toe.CircularArrayQueue
 			{
 				return 0;
 			}
+            
 			int length = 0;
-			foreach (var currentChar in str)
-			{
-				if (currentChar == 0)
-				{
-					throw new ArgumentException("String can not contain \\0 character");
-				}
-				if (currentChar < '\x80')
-				{
-					++length;
-				}
-				else if (currentChar < '\x800')
-				{
-					length += 2;
-				}
-				else if (currentChar < 0x10000)
-				{
-					length += 3;
-				}
-				else if (currentChar < 0x200000)
-				{
-					length += 4;
-				}
-				else if (currentChar < 0x4000000)
-				{
-					length += 5;
-				}
-				else
-				{
-					length += 6;
-				}
-			}
-			return length;
+		    for (int index = 0; index < str.Length; index++)
+		    {
+		        var currentChar = str[index];
+		        if (currentChar == 0)
+		        {
+		            throw new ArgumentException("String can not contain \\0 character");
+		        }
+		        if (currentChar < '\x80')
+		        {
+		            ++length;
+		        }
+		        else if (currentChar < '\x800')
+		        {
+		            length += 2;
+		        }
+		        else if (currentChar < '\uD800' || currentChar > '\uDFFF')
+		        {
+		            length += 3;
+		        }
+		        else if (currentChar <= '\uDBFF')
+		        {
+                    if (index + 1 < str.Length && str[index + 1] >= '\uDC00' && str[index + 1] <= '\uDFFF')
+                    {
+                        ++index;
+                        length += 4;
+                    }
+                    else
+                    {
+                        throw new NotImplementedException();
+                    }
+		        }
+		        else
+		        {
+		            throw new NotImplementedException();
+		        }
+		    }
+		    return length;
 		}
 
 		public static int GetStringLength(this IMessageQueue messageQueue, string text)
@@ -145,36 +151,92 @@ namespace Toe.CircularArrayQueue
 		public static int WriteStringContent(this IMessageQueue messageQueue, int contentIndex, string str)
 		{
 			str = str ?? String.Empty;
+		    int buffer = 0;
+		    int shift = 0;
+            for (int index = 0; index < str.Length; ++index)
+            {
+                int ch = str[index];
+                if (ch < '\x80')
+                {
+                    AddByte((byte)ch, ref buffer, ref shift, ref contentIndex, messageQueue);
+					} else if (ch < '\x800') {
+                        AddByte((byte) (0xC0 | (ch >> 6)), ref buffer, ref shift, ref contentIndex, messageQueue);
+                        AddByte((byte) (0x80 | (ch & 0x3F)), ref buffer, ref shift, ref contentIndex, messageQueue);
+                    }
+                else if (ch < '\uD800' || ch > '\uDFFF')
+                {
+                    AddByte((byte)(0xE0 | (ch >> 12)), ref buffer, ref shift, ref contentIndex, messageQueue);
+                    AddByte((byte)(0x80 | ((ch >> 6) & 0x3F)), ref buffer, ref shift, ref contentIndex, messageQueue);
+                    AddByte((byte)(0x80 | (ch & 0x3F)), ref buffer, ref shift, ref contentIndex, messageQueue);
+                }
+                else if (ch <= '\uDBFF')
+                {
+                    if (index + 1 < str.Length && str[index + 1] >= '\uDC00' && str[index + 1] <= '\uDFFF')
+                    {
+                        ch = 0x10000 + (int)str[index + 1] - 0xDC00 + (((int)str[index] - 0xD800) << 10);
+                        AddByte((byte)(0xF0 | (ch >> 18)), ref buffer, ref shift, ref contentIndex, messageQueue);
+                        AddByte((byte)(0x80 | ((ch >> 12) & 0x3F)), ref buffer, ref shift, ref contentIndex, messageQueue);
+                        AddByte((byte)(0x80 | ((ch >> 6) & 0x3F)), ref buffer, ref shift, ref contentIndex, messageQueue);
+                        AddByte((byte)(0x80 | (ch & 0x3F)), ref buffer, ref shift, ref contentIndex, messageQueue);
+                        ++index;
+                    }
+                    else
+                    {
+                        throw new NotImplementedException();
+                    }
+                }
+                else
+                {
+                    throw new NotImplementedException();
+                }
+            }
+            AddByte(0, ref buffer, ref shift, ref contentIndex, messageQueue);
+            if (shift != 0)
+                messageQueue.WriteInt32(contentIndex, buffer);
+            return contentIndex + 1;
 
-			//TODO: avoid array allocation.
-			var bytes = Encoding.UTF8.GetBytes(str);
-			int index = 0;
-			for (; index + 3 < bytes.Length; index += 4)
-			{
-				messageQueue.WriteInt32(
-					contentIndex, (bytes[index]) | (bytes[index + 1] << 8) | (bytes[index + 2] << 16) | (bytes[index + 3] << 24));
-				++contentIndex;
-			}
-			int val = 0;
-			if (index < bytes.Length)
-			{
-				val |= (bytes[index]);
-				++index;
-				if (index < bytes.Length)
-				{
-					val |= (bytes[index]) << 8;
-					++index;
-					if (index < bytes.Length)
-					{
-						val |= (bytes[index]) << 16;
-					}
-				}
-			}
-			messageQueue.WriteInt32(contentIndex, val);
-			++contentIndex;
-			return contentIndex;
+            ////TODO: avoid array allocation.
+            //var bytes = Encoding.UTF8.GetBytes(str);
+            //int index = 0;
+            //for (; index + 3 < bytes.Length; index += 4)
+            //{
+            //    messageQueue.WriteInt32(
+            //        contentIndex, (bytes[index]) | (bytes[index + 1] << 8) | (bytes[index + 2] << 16) | (bytes[index + 3] << 24));
+            //    ++contentIndex;
+            //}
+            //int val = 0;
+            //if (index < bytes.Length)
+            //{
+            //    val |= (bytes[index]);
+            //    ++index;
+            //    if (index < bytes.Length)
+            //    {
+            //        val |= (bytes[index]) << 8;
+            //        ++index;
+            //        if (index < bytes.Length)
+            //        {
+            //            val |= (bytes[index]) << 16;
+            //        }
+            //    }
+            //}
+            //messageQueue.WriteInt32(contentIndex, val);
+            //++contentIndex;
+            //return contentIndex;
 		}
 
-		#endregion
+	    private static void AddByte(byte ch, ref int buffer, ref int shift, ref int contentIndex, IMessageQueue messageQueue)
+	    {
+	        buffer |= ((int)ch) << shift;
+	        shift += 8;
+            if (shift == 32)
+            {
+                messageQueue.WriteInt32(contentIndex,buffer);
+                shift = 0;
+                buffer = 0;
+                ++contentIndex;
+            }
+	    }
+
+	    #endregion
 	}
 }
